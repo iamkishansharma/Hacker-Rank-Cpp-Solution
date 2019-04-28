1d0e691d606c1308916cdf041b8b2f89b849c332
Kishan Kr Sharma <36340195+iamkishansharma@users.noreply.github.com>
2019-04-28 23:27:14 +0530

    Create 7 c-tutorial-pointer.cpp

---
+++ 7 c-tutorial-pointer.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void update(int *a,int *b) {
+    // Complete this function 
+    int n = *a;
+    *a = *a+*b;
+    *b = n-*b;
+    if(*b < 0){
+        *b =*b * -1;
+    }
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
+
