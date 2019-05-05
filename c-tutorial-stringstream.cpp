b09c30e5ab355e1e1d68cfb6292b59f9ff786ca3
Kishan Kr Sharma <36340195+iamkishansharma@users.noreply.github.com>
2019-05-05 23:54:15 +0530

    Create c-tutorial-stringstream.cpp

---
+++ c-tutorial-stringstream.cpp
@@ -0,0 +1,34 @@
+#include <sstream>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<int> parseInts(string str) {
+	// Complete this function
+    vector<int> integers;
+    stringstream ss(str);
+    bool first = true;
+    char ch;
+    int integer;
+    while(!ss.eof()){
+        if(!first){
+            ss >> ch;
+        }
+        ss >> integer;
+        integers.push_back(integer);
+        first = false;
+    }
+    return integers;
+}
+
+int main() {
+    string str;
+    cin >> str;
+    vector<int> integers = parseInts(str);
+    for(int i = 0; i < integers.size(); i++) {
+        cout << integers[i] << "\n";
+    }
+    
+    return 0;
+}
+
